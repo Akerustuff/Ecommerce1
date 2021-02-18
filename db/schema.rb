71fc90c932195cf2a923f2067b75142d0f6bd745
Kelvis <kelvis.alvarez20@gmail.com>
2021-02-18 19:03:05 -0300

    Remove stock from Product Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_014609) do
+ActiveRecord::Schema.define(version: 2021_02_18_215949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,7 +92,6 @@ ActiveRecord::Schema.define(version: 2021_02_18_014609) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.integer "stock"
     t.decimal "price"
     t.string "sku"
     t.datetime "created_at", null: false
